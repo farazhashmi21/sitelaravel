e7c132177fc603cb3183974b979ea12057ac4dc2
DeveloperFaraz6465 <farazhashmi9000@gmail.com>
2022-04-09 19:35:25 +0000

    first commit

---
+++ siteLaravel.sh
@@ -0,0 +1,10 @@
+echo "# sitelaravel";
+echo $(git init);
+echo $(git add .);
+echo $(git commit -m "first commit");
+echo $(git branch -M main);
+echo $(git remote add origin https://github.com/farazhashmi21/sitelaravel.git);
+echo $(git remote -v);
+echo $(git push -u origin main);
+echo "# Repository Updated.";
+
