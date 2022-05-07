90e543b1794629cf58d30908fad3821613501b06
DeveloperFaraz6465 <farazhashmi9000@gmail.com>
2022-05-07 18:32:02 +0000

    Project Updated.

--- siteLaravel.sh
+++ siteLaravel.sh
@@ -2,7 +2,7 @@ echo "#Project: sitelaravel";
 echo $(git gc);
 echo $(git init);
 echo $(git add .);
-echo $(git commit -m "1) Wellcome Controller Created, 2) Basic Blade Template Created, 3) Data shared from contorller to view successfully.");
+echo $(git commit -m "Project Updated.");
 echo $(git branch -M main);
 echo $(git remote -v);
 echo $(git push -u origin main);
